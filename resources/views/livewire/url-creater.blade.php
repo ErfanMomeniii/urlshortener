120979abbb962d791f9199d4da6446f48dccd59b
Erfan <erfammioi@gmail.com>
2022-03-09 13:29:57 +0330

    handle indent problem and designed

--- resources/views/livewire/url-creater.blade.php
+++ resources/views/livewire/url-creater.blade.php
@@ -1,28 +1,33 @@
 <div>
     {{-- If your happiness depends on money, you will never be happy with yourself. --}}
 
-  <div wire:ignore.self class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Url creater</h5>
-          <button wire:click="cancel" type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-            <input wire:model="url" name="url" type="search" class="form-control" placeholder="..." aria-label="Search">
-            @if($newShortUrl!=="")
+    <div wire:ignore.self class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Url creater</h5>
+                    <button wire:click="cancel" type="button" class="btn-close" data-bs-dismiss="modal"
+                        aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <input wire:model="url" name="url" type="search" class="form-control" placeholder="..."
+                        aria-label="Search">
+                    @if ($newShortUrl !== '')
+                        <span class="text-success">Your shorter url is <b>{{ $newShortUrl }}</b></span>
+                    @endif
 
-            <span class="text-success">Your shorter url is <b>{{$newShortUrl}}</b></span>
-            @endif
+                    @error('url')
+                        <span class="error text-danger">{{ $message }}</span>
+                    @enderror
 
-            @error('url') <span class="error text-danger">{{ $message }}</span> @enderror
-
+                </div>
+                <div class="modal-footer">
+                    <button wire:click="add" type="button" class="btn btn-success">Add</button>
+                    <button wire:click="cancel" type="button" class="btn btn-danger"
+                        data-bs-dismiss="modal">Cancel</button>
+                </div>
+            </div>
         </div>
-        <div class="modal-footer">
-            <button wire:click="add" type="button" class="btn btn-success">Add</button>
-          <button wire:click="cancel" type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancel</button>
-        </div>
-      </div>
     </div>
-  </div>
 </div>
--- resources/views/panel/index.blade.php
+++ resources/views/panel/index.blade.php
@@ -1,33 +1,30 @@
-
 @extends('layout.layout')
 @section('header')
+    <div class="px-3 py-2 bg-dark  text-white">
+        <div class="container">
+            <div class="d-flex flex-wrap align-items-center justify-content-center ">
 
-<div class="px-3 py-2 bg-dark  text-white">
-    <div class="container">
-      <div class="d-flex flex-wrap align-items-center justify-content-center ">
-
-        <ul class="nav col-12 col-lg-auto my-2 justify-content-center text-align-center my-md-0 text-small">
-          <li>
-            <h1 class="nav-link text-white ">
-              URL-SHORTENER
-            </h1>
-          </li>
-        </ul>
-      </div>
+                <ul class="nav col-12 col-lg-auto my-2 justify-content-center text-align-center my-md-0 text-small">
+                    <li>
+                        <h1 class="nav-link text-white ">
+                            URL-SHORTENER
+                        </h1>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
-  </div>
 @endsection
 @section('content')
-<div class="p-5 d-flex justify-content-end align-items-center">
-    <!-- Button trigger modal -->
-<button type="button" class="btn btn-info" data-bs-toggle="modal" data-bs-target="#exampleModal">
-ADD URL
-</button>
+    <div class="p-5 d-flex justify-content-end align-items-center">
+        <!-- Button trigger modal -->
+        <button type="button" class="btn btn-info" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            ADD URL
+        </button>
 
-<!-- Modal -->
-@livewire('url-creater')
-</div>
+        <!-- Modal -->
+        @livewire('url-creater')
+    </div>
 
-@livewire('url-redirecter')
-
+    @livewire('url-redirecter')
 @endsection
