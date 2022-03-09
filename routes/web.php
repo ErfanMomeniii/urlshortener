097778be6129f8b77d89874c19d636d208f8a712
Erfan <erfammioi@gmail.com>
2022-03-09 13:31:45 +0330

    add new route for handle redirect with url

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Services\UrlService;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('panel.index');
+});
+
+Route::get('/{shortUrl}', function ($shortUrl) {
+    return redirect()->away(UrlService::getUrl($shortUrl));
 });
