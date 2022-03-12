4b20430dd2a24b15457da76f3f5723c04da5e208
Erfan <erfammioi@gmail.com>
2022-03-12 10:50:10 +0330

    add feature test for api methods

---
+++ tests/Feature/UrlShortenerApiTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Http\Response;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UrlShortenerApiTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_if_get_urls_with_api_work()
+    {
+
+        $this->json('get', 'api/url')
+             ->assertStatus(Response::HTTP_OK)
+             ->assertJsonStructure(
+                 [
+                     'data' => [
+                         '*' => [
+                             'URL',
+                             'Short_URL'
+                         ]
+                             ],
+                             'status'
+                 ]
+             );
+    }
+
+    public function test_if_add_url_with_api_work(){
+        $response=$this->post( 'api/url' ,[
+            'url'=>'https://liinkedin.com/signup'
+        ]);
+        $this->assertDatabaseHas('urls',[
+            'URL'=>'https://liinkedin.com/signup',
+        ]);
+
+    }
+
+}
