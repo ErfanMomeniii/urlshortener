74e63cd0949c5802b55f55d5f4f36fc36aa07bc6
Erfan <erfammioi@gmail.com>
2022-04-04 14:36:38 +0430

    complete user controller test

--- tests/Feature/UserControllerTest.php
+++ tests/Feature/UserControllerTest.php
@@ -43,9 +43,7 @@ class UserControllerTest extends TestCase
         $this->withHeaders([
             'Accept' => 'application/json',
             'Authorization' => $userToken,
-        ])->get('api/user/' . $otherUser->id, [
-            'path' => 'https://linkedi1n.com/signup'
-        ])->assertJson([
+        ])->get('api/user/' . $otherUser->id)->assertJson([
             'username' => $otherUser->username,
             'password' => $otherUser->password
         ]);
@@ -62,45 +60,137 @@ class UserControllerTest extends TestCase
         /**
          * @var User $otherUser
          */
-        $user = app()->make(UserFactory::class)->create();;
+        $user = app()->make(UserFactory::class)->create();
         $userToken = app()->make(TokenService::class)->generateUserToken($user);
         $otherUser = app()->make(UserFactory::class)->create();
 
         $this->withHeaders([
             'Accept' => 'application/json',
             'Authorization' => $userToken,
-        ])->get('api/user/' . $otherUser->id, [
-            'path' => 'https://linkedi1n.com/signup'
-        ])->assertStatus(403);
+        ])->get('api/user/' . $otherUser->id)->assertStatus(403);
     }
 
+    /**
+     * @throws SigningException
+     * @throws JsonEncodingException
+     * @throws BindingResolutionException
+     * @throws InvalidKeyException
+     */
     public function test_user_with_access_see_other_user_with_invalid_token_should_fail()
     {
+        /**
+         * @var User $otherUser
+         */
+        $user = $this->fakeAdminWithPermissions();
+        $userToken = app()->make(TokenService::class)->generateUserToken($user);
+        $otherUser = app()->make(UserFactory::class)->create();
 
+        $this->withHeaders([
+            'Accept' => 'application/json',
+            'Authorization' => $userToken . 'a',
+        ])->get('api/user/' . $otherUser->id)->assertStatus(401);
     }
 
+    /**
+     * @throws SigningException
+     * @throws JsonEncodingException
+     * @throws BindingResolutionException
+     * @throws InvalidKeyException
+     */
     public function test_user_with_access_see_notfound_user_should_fail()
     {
+        $user = $this->fakeAdminWithPermissions();
+        $userToken = app()->make(TokenService::class)->generateUserToken($user);
+        $otherUser = app()->make(UserFactory::class)->create();
 
+        $this->withHeaders([
+            'Accept' => 'application/json',
+            'Authorization' => $userToken,
+        ])->get('api/user/' . 'aaa')->assertStatus(404);
     }
 
+    /**
+     * @throws SigningException
+     * @throws JsonEncodingException
+     * @throws BindingResolutionException
+     * @throws InvalidKeyException
+     */
     public function test_user_with_right_access_delete_other_user_should_work()
     {
+        /**
+         * @var User $otherUser
+         */
+        $user = $this->fakeAdminWithPermissions();
+        $userToken = app()->make(TokenService::class)->generateUserToken($user);
+        $otherUser = app()->make(UserFactory::class)->create();
 
+        $this->withHeaders([
+            'Accept' => 'application/json',
+            'Authorization' => $userToken,
+        ])->delete('api/user/' . $otherUser->id)->assertStatus(200);
     }
 
+    /**
+     * @throws SigningException
+     * @throws JsonEncodingException
+     * @throws BindingResolutionException
+     * @throws InvalidKeyException
+     */
     public function test_user_without_access_delete_other_user_should_fail()
     {
+        /**
+         * @var User $otherUser
+         */
+        $user = app()->make(UserFactory::class)->create();
+        $userToken = app()->make(TokenService::class)->generateUserToken($user);
+        $otherUser = app()->make(UserFactory::class)->create();
 
+        $this->withHeaders([
+            'Accept' => 'application/json',
+            'Authorization' => $userToken,
+        ])->delete('api/user/' . $otherUser->id)->assertStatus(403);
     }
 
+    /**
+     * @throws SigningException
+     * @throws JsonEncodingException
+     * @throws BindingResolutionException
+     * @throws InvalidKeyException
+     */
     public function test_user_with_access_delete_other_user_with_invalid_token_should_fail()
     {
+        /**
+         * @var User $otherUser
+         */
+        $user = $this->fakeAdminWithPermissions();
+        $userToken = app()->make(TokenService::class)->generateUserToken($user);
+        $otherUser = app()->make(UserFactory::class)->create();
 
+        $this->withHeaders([
+            'Accept' => 'application/json',
+            'Authorization' => $userToken . 'a',
+        ])->delete('api/user/' . $otherUser->id)->assertStatus(401);
     }
 
+    /**
+     * @throws SigningException
+     * @throws JsonEncodingException
+     * @throws BindingResolutionException
+     * @throws InvalidKeyException
+     */
     public function test_user_with_access_delete_notfound_user_should_fail()
     {
+        /**
+         * @var User $otherUser
+         */
+        $user = $this->fakeAdminWithPermissions();
+        $userToken = app()->make(TokenService::class)->generateUserToken($user);
+        $otherUser = app()->make(UserFactory::class)->create();
+
+        $this->withHeaders([
+            'Accept' => 'application/json',
+            'Authorization' => $userToken,
+        ])->delete('api/user/' . 'aaa')->assertStatus(404);
 
     }
 }
